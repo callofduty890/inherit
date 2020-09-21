6c53db01cb5c52b1a4fea072d6ac05505d0555dd
俊华 蔡 <1032031912@qq.com>
2020-09-21 09:15:51 +0800

    添加项目文件。

---
+++ 9_21_继承/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLHelper;//引用自己写好的数据库DLL
+
+namespace _9_21_继承
+{
+    //定义一个数据类
+    class AdminLogin
+    {
+        private string account;//账号
+        private string passwords;//密码
+        private string powers;//权限
+
+        public string Account { get => account; set => account = value; }
+        public string Passwords { get => passwords; set => passwords = value; }
+        public string Powers { get => powers; set => powers = value; }
+    }
+
+    class Program
+    {
+        /// <summary>
+        /// 验证账号密码
+        /// </summary>
+        /// <param name="objAdmin">数据类</param>
+        /// <returns>数据对象(NULL/账号密码权限)</returns>
+        public static AdminLogin Login(AdminLogin objAdmin)
+        {
+            //形成SQL语句
+            string sql = "select powers from information where account='{0}' and passwords='{1}'";
+            sql = string.Format(sql,objAdmin.Account, objAdmin.Passwords);
+
+            //向数据库发起请求
+            SqlDataReader objReader= SQLHelper.SQLHelper.GetReader(sql);
+
+            //判断是否有内容
+            if (objReader.Read())
+            {
+                objAdmin.Powers = (string)objReader["powers"];//获取对应的权限
+                return objAdmin;//取到对应的值，返回对象(账号/密码/权限)
+            }
+            else
+            {
+                return objAdmin = null;//返回NULL说明没有对应的账号
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            //提示输入账号与密码
+            Console.WriteLine("请输入账号:");
+            string account=  Console.ReadLine();
+            Console.WriteLine("请输入密码:");
+            string password = Console.ReadLine();
+
+            //创建数据对象
+            AdminLogin admin = new AdminLogin();
+            admin.Account = account;
+            admin.Passwords = password;
+
+            //验证账号和密码数据库中是否存在
+            admin = Login(admin);
+            if (admin != null)
+            {
+                Console.WriteLine("登录成功!,您的权限为{0}", admin.Powers);
+            }
+            else
+            {
+                Console.WriteLine("登录失败,请检查账号密码");
+            }
+
+            Console.ReadLine();//卡屏
+        }
+    }
+}
---
+++ SQLHelper/SQLHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLHelper
+{
+    public class SQLHelper
+    {
+        //编辑SQL Server数据库连接字符串
+        readonly static string conString = @"Server=DESKTOP-CTV4ATU\SQLSERVER;DataBase=Admin_information;Uid=sa;pwd=123456";
+
+        /// <summary>
+        /// 执行 SQL查询命令，获取SqlDataReader结果对象
+        /// </summary>
+        /// <param name="sql">SQL命令</param>
+        /// <returns></returns>
+        public static SqlDataReader GetReader(string sql)
+        {
+            SqlConnection conn = new SqlConnection(conString);//连接数据库
+            SqlCommand cmd = new SqlCommand(sql, conn);//执行SQL语句
+            try
+            {
+                conn.Open();//打开数据库
+                return cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);//返回对象
+            }
+            catch (Exception)
+            {
+                //异常处理
+                conn.Close();//关闭数据库
+                throw;//错误信息抛出
+            }
+        }
+
+        /// <summary>
+        /// 执行 增删改 方法
+        /// </summary>
+        /// <param name="sql">SQL语句</param>
+        /// <returns>返回受影响的行数</returns>
+        public static int Update(string sql)
+        {
+            SqlConnection conn = new SqlConnection(conString);//连接数据库
+            SqlCommand cmd = new SqlCommand(sql, conn);//执行SQL语句
+            try
+            {
+                conn.Open();
+                return cmd.ExecuteNonQuery();//返回受影响的行数
+            }
+            catch (Exception)
+            {
+                //可以将错误的日志写入保存到txt当中
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+    }
+}
